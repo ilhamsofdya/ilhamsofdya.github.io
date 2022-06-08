3504eac94a1bc31fd00aab3b20a4a771ce58a02d
ilhamsofdya <42112832+ilhamsofdya@users.noreply.github.com>
2022-06-08 21:11:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
-$path = "https://api.telegram.org/bot5355398612:AAHtMgc6a-0gGR4U4uByVYU13a3rQqbe03k;
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-$update = json_decode(file_get_contents("php://input"), TRUE);
+class Bot extends CI_Controller {
+    function __construct(){
+        parent::__construct();
+    }
 
-$chatId = $update["message"]["chat"]["id"];
-$message = $update["message"]["text"];
-
-if (strpos($message, "/weather") === 0) {
-$location = substr($message, 9);
-$weather = "Iya";
-file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+    function index(){
+        $TOKEN = "5534137874:AAG0s-FK0w1IJTZ109HMknxfTucSZKXh8nw";
+        $apiURL = "https://api.telegram.org/bot$TOKEN";
+        $update = json_decode(file_get_contents("php://input"), TRUE);
+        $chatID = $update["message"]["chat"]["id"];
+        $message = $update["message"]["text"];
+        
+        if (strpos($message, "/start") === 0) {
+        
+        file_get_contents($apiURL."/sendmessage?chat_id=".$chatID."&text=Haloo, test webhooks dicoffeean.com.&parse_mode=HTML"); 
+        } 
+    } 
 }
-?>

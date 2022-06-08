f1211618d9fe52956bc8a980112c17504bc38f52
ilhamsofdya <42112832+ilhamsofdya@users.noreply.github.com>
2022-06-08 21:32:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,53 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Bot extends CI_Controller {
-    function __construct(){
-        parent::__construct();
-    }
+Result Size: 1346 x 202
 
-    function index(){
-        $TOKEN = "5534137874:AAG0s-FK0w1IJTZ109HMknxfTucSZKXh8nw";
-        $apiURL = "https://api.telegram.org/bot$TOKEN";
-        $update = json_decode(file_get_contents("php://input"), TRUE);
-        $chatID = $update["message"]["chat"]["id"];
-        $message = $update["message"]["text"];
-        
-        if (strpos($message, "/start") === 0) {
-        
-        file_get_contents($apiURL."/sendmessage?chat_id=".$chatID."&text=Haloo, test webhooks dicoffeean.com.&parse_mode=HTML"); 
-        } 
-    } 
-}
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+  <title>Bypass Link</title>
+
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+​
+
+<div class="container">
+
+  <h2>Bypass Link</h2>
+
+  <form action="/proses.php">
+
+    <div class="form-group">
+
+      <label for="link">Link:</label>
+
+      <input type="link" class="form-control" id="link" placeholder="Enter link" name="link">
+
+    <button type="submit" class="btn btn-default">Submit</button>
+
+  </form>
+
+</div>
+
+​
+
+</body>
+
+</html>
+
+​
+

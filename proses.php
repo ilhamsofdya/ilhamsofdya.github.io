16582042e683dc4749d8e71c6fefe9458f9c0ecd
ilhamsofdya <42112832+ilhamsofdya@users.noreply.github.com>
2022-06-08 21:37:25 +0700

    Create proses.php

---
+++ proses.php
@@ -0,0 +1,11 @@
+<?php 
+    //proses yang dapat terjadi
+    //mengambil data dari parameter input halaman  biodata
+    $link = $_POST['link'];
+    //menampilkan data 
+    echo "Nama = $link"."<br>";
+    //disini ditulis proses lain yang dapat dilakukan seperti
+    
+    //insert ke database
+    //update delete data
+?>
